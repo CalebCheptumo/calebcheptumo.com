924ea4c27345f8c0cd92f20ad4707ad5a0bf488d
calebcheptumo <ckibusia@gmail.com>
2023-12-20 13:30:13 +0300

    feat: Add Projects, Blog, and Contact links to navigation

--- src/components/header.js
+++ src/components/header.js
@@ -61,9 +61,56 @@ const Header = ({ siteTitle }) => (
             About
           </Link>
         </li>
+        <li
+          style={{
+            marginLeft: `var(--space-4)`,
+          }}
+        >
+          <Link
+            to="/projects"
+            style={{
+              fontSize: `var(--font-md)`,
+              textDecoration: `none`,
+              color: `var(--color-text)`,
+            }}
+          >
+            Projects
+          </Link>
+        </li>
+        <li
+          style={{
+            marginLeft: `var(--space-4)`,
+          }}
+        >
+          <Link
+            to="/blog"
+            style={{
+              fontSize: `var(--font-md)`,
+              textDecoration: `none`,
+              color: `var(--color-text)`,
+            }}
+          >
+            Blog
+          </Link>
+        </li>
+        <li
+          style={{
+            marginLeft: `var(--space-4)`,
+          }}
+        >
+          <Link
+            to="/contact"
+            style={{
+              fontSize: `var(--font-md)`,
+              textDecoration: `none`,
+              color: `var(--color-text)`,
+            }}
+          >
+            Contact
+          </Link>
+        </li>
       </ul>
     </nav>
-    
   </header>
 )
 
