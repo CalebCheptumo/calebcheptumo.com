6ba1700ff0c97f2950019486cacdb7fe31dc171d
calebcheptumo <ckibusia@gmail.com>
2023-12-20 17:23:36 +0300

    style: Styled hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,36 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { StaticImage } from "gatsby-plugin-image"
-import * as styles from "../components/index.module.css"
+import {hero ,heroText, heading,subheading,buttons,button,buttonWork} from "../components/index.module.css"
 
 const IndexPage = () => (
   <Layout>
     {/*start of hero*/}
-    <div className={styles.hero}>
-      <div className={styles.heroText}>
-        <h1>Hi, I'm Caleb Cheptumo, a Web Developer</h1>
-        <p>
+    <div className={hero}>
+      <div className={heroText}>
+        <h1 className={heading}>Hi, I'm Caleb Cheptumo, a Web Developer</h1>
+        <p className={subheading}>
           Specializing in building exceptional, high-quality websites and
           applications.
         </p>
-        <a
-          href="https://drive.google.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className={styles.button}>Download Resume</button>
-        </a>
-      </div>
-      <div className={styles.heroImage}>
-        <StaticImage
-          src="../images/calebcheptumo.png"
-          alt="Caleb Cheptumo"
-          style={{ borderRadius: "50%", width: "300px", height: "300px" }}
-        />
+        <div className={buttons}>
+          <a
+            href="https://drive.google.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className={button}>Download Resume</button>
+          </a>
+          <a
+            href="mailto:kibusia@calebcheptumo.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className={buttonWork}>
+              Let's Work Together
+            </button>
+          </a>
+        </div>
       </div>
     </div>
     {/*end of hero*/}
